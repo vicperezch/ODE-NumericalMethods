9ad8673efbd3d01b89018f39a3001fcaaf18ef8b
Victor <vmpch1@outlook.com>
2024-11-07 20:43:59 -0600

    Añadido método Adams-Bashforth-Moulton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from numerical_methods import rk3, graph
+from numerical_methods import rk3, abm, graph
 
 # Ecuación diferencial a resolver: y' = f(x, y)
 def f(x: float, y: float) -> float:
@@ -6,8 +6,10 @@ def f(x: float, y: float) -> float:
 
 
 def main():
-    x, y = rk3(f, 0, 0.5, 2, 0.01)
-    graph(x, y)
+    x1, y1 = rk3(f, 0, 0.5, 2, 0.01)
+    x2, y2 = abm(f, 0, 0.5, 2, 0.01)
+    graph(x1, y1)
+    graph(x2, y2)
 
 
 if __name__ == '__main__':
--- numerical_methods.py
+++ numerical_methods.py
@@ -32,3 +32,31 @@ def rk3(f, x_0: float, y_0: float, x_f: float, h: float) -> typing.Tuple[np.ndar
         y[i] = y[i - 1] + (k1 + 4 * k2 + k3) * h / 6
     
     return x, y
+
+
+"""
+Método de Adams-Bashforth-Moulton
+f: función que representa la ecuación diferencial
+y_0: valor inicial de y
+x_0: valor inicial de x
+x_f: valor final de x
+h: paso
+"""
+def abm(f, x_0: float, y_0: float, x_f: float, h: float):
+    x = np.arange(x_0, x_f, h)
+    y = np.zeros(x.size)
+
+    np.put(y, [0, 1, 2, 3], (rk3(f, x_0, y_0, x_0 + 4 * h, h)[1]))
+
+    for i in range(4, x.size):
+        y_1 = f(x[i - 1], y[i - 1])
+        y_2 = f(x[i - 2], y[i - 2])
+        y_3 = f(x[i - 3], y[i - 3])
+        y_4 = f(x[i - 4], y[i - 4])
+
+        pred = y[i - 1] + (55 * y_1 - 59 * y_2 + 37 * y_3 - 9 * y_4) * h / 24
+        corr = y[i - 1] + (9 * f(x[i], pred) + 19 * y_1 - 5 * y_2 + y_3) * h / 24
+
+        y[i] = corr
+    
+    return x, y
